550e301dbabd825b5ccc78d1a853aca0deaa843c
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-07-16 01:17:01 +0800

    3518 Add splash activity full screen

--- app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/SplashActivity.java
+++ app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/SplashActivity.java
@@ -2,7 +2,9 @@ package com.ixuea.cources.mycloudmusicproject.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ixuea.cources.mycloudmusicproject.R;
 
@@ -13,5 +15,24 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //设置界面全屏启动
+        //获取decorView
+        View decorView = getWindow().getDecorView();
+
+        //判断版本
+        if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) {
+            //11~18版本
+            decorView.setSystemUiVisibility(View.GONE);
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            //19及以上版本
+            //SYSTEM_UI_FLAG_HIDE_NAVIGATION:隐藏导航栏
+            //SYSTEM_UI_FLAG_IMMERSIVE_STICKY:从状态栏下拉会半透明悬浮显示一会儿状态栏和导航栏
+            //SYSTEM_UI_FLAG_FULLSCREEN:全屏
+            int options = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+            //设置控件
+            decorView.setSystemUiVisibility(options);
+        }
     }
 }
