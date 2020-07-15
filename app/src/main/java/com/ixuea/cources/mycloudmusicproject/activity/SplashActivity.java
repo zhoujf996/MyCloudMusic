2f215991305f6feeee12cf9cf3d7069959888eec
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-07-16 01:44:18 +0800

    3510 Add splash activity delay

--- app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/SplashActivity.java
+++ app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/SplashActivity.java
@@ -1,15 +1,42 @@
 package com.ixuea.cources.mycloudmusicproject.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 
 import com.ixuea.cources.mycloudmusicproject.R;
 
 //启动界面
 public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = "SplashActivity";
+    //下一步常量
+    private static final int MESSAGE_NEXT = 100;
+    //默认延时时间
+    private static final long DEFAULT_DELAY_TIME = 3000;
+    
+    //创建Handler
+    //这样创建会有内存泄漏，以后会性能优化
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(@NonNull Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case MESSAGE_NEXT:
+                    next();
+                    break;
+            }
+        }
+    };
+
+    private void next() {
+        Log.d(TAG, "next() ");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,5 +61,15 @@ public class SplashActivity extends AppCompatActivity {
             //设置控件
             decorView.setSystemUiVisibility(options);
         }
+
+        ////延时3秒
+        ////在企业中通常会有很多逻辑处理
+        ////所以延时时间最好是用3-消耗的的时间
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                handler.sendEmptyMessage(MESSAGE_NEXT);
+            }
+        }, DEFAULT_DELAY_TIME);
     }
 }
