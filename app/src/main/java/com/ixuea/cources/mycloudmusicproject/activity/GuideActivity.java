692965585fdaf9111894b9fade6fb82093b3b46a
zhoujy0701 <zhoujy0701@thundersoft.com>
2020-07-16 17:14:20 +0800

    3522 Add guide activity button click

--- app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/GuideActivity.java
+++ app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/GuideActivity.java
@@ -1,14 +1,25 @@
 package com.ixuea.cources.mycloudmusicproject.activity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.ixuea.cources.mycloudmusicproject.R;
 
 //引导界面
-public class GuideActivity extends AppCompatActivity {
+public class GuideActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "GuideActivity";
+    //快捷键ctrl+alt+f快速生成
+    //登录注册按钮
+    private Button bt_login_or_register;
+    private Button bt_enter;
+
     //当界面创建的时候
     //一定要注意：
     //要用onCreate的方法参数是Bundle的这个方法
@@ -23,5 +34,31 @@ public class GuideActivity extends AppCompatActivity {
         //隐藏状态栏
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //找控件
+        //登录注册按钮
+        bt_login_or_register = findViewById(R.id.bt_login_or_register);
+        //立即注册按钮
+        bt_enter = findViewById(R.id.bt_enter);
+
+        //设置点击事件
+        //登录注册按钮
+        bt_login_or_register.setOnClickListener(this);
+        //立即体验按钮
+        bt_enter.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //因为有很多个按钮都设置同一个监听器
+        //所以这里需要区分到底是哪一个按钮点击
+        switch (view.getId()) {
+            case R.id.bt_login_or_register:
+                Log.d(TAG, "onClick login or register");
+                break;
+            case R.id.bt_enter:
+                Log.d(TAG, "onClick enter");
+                break;
+        }
     }
 }
