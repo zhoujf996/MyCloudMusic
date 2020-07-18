451684b5f64eb1c8a5a93468707ab69ccf411bb5
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-07-18 11:49:06 +0800

    3563 Add tool bar

--- app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/LoginActivity.java
+++ app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/LoginActivity.java
@@ -1,17 +1,28 @@
 package com.ixuea.cources.mycloudmusicproject.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import androidx.appcompat.widget.Toolbar;
 import com.ixuea.cources.mycloudmusicproject.R;
+import butterknife.BindView;
 
 //登录界面
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseCommonActivity {
+
+    //标题控件
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        //初始化Toolbar
+        setSupportActionBar(toolbar);
     }
 }
