18123670ca332b072c49fb9eec865caec351eae7
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-07-18 11:09:06 +0800

    3562 Add register click

--- app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/LoginOrRegisterActivity.java
+++ app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/LoginOrRegisterActivity.java
@@ -1,28 +1,25 @@
 package com.ixuea.cources.mycloudmusicproject.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-
 import com.ixuea.cources.mycloudmusicproject.R;
-
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 
 //登录注册界面
-public class LoginOrRegisterActivity extends BaseCommonActivity implements View.OnClickListener {
+public class LoginOrRegisterActivity extends BaseCommonActivity {
 
     private static final String TAG = "LoginOrRegisterActivity";
     //登录按钮
     @BindView(R.id.bt_login)
     Button bt_login;
     //注册按钮
-    private Button bt_register;
+//    private Button bt_register;
+    @BindView(R.id.bt_register)
+    Button bt_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,15 +30,14 @@ public class LoginOrRegisterActivity extends BaseCommonActivity implements View.
     @Override
     protected void initViews() {
         super.initViews();
-        
+
         //显示亮色状态栏
         lightStatusBar();
-
 
         //登录按钮
 //        bt_login = findViewById(R.id.bt_login);
         //注册按钮
-        bt_register = findViewById(R.id.bt_register);
+//        bt_register = findViewById(R.id.bt_register);
     }
 
     @Override
@@ -50,31 +46,39 @@ public class LoginOrRegisterActivity extends BaseCommonActivity implements View.
         //登录按钮监听器
 //        bt_login.setOnClickListener(this);
         //注册按钮监听器
-        bt_register.setOnClickListener(this);
+//        bt_register.setOnClickListener(this);
     }
 
     //点击回调方法
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            //登录按钮
+//    @Override
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            //登录按钮
 //            case R.id.bt_login:
 //                Log.d(TAG, "onClick: login");
 //
 //                startActivity(LoginActivity.class);
 //                break;
-            //注册按钮
-            case R.id.bt_register:
-                Log.d(TAG, "onClick: register");
-
-                startActivity(RegisterActivity.class);
-                break;
-        }
-    }
+//            //注册按钮
+//            case R.id.bt_register:
+//                Log.d(TAG, "onClick: register");
+//
+//                startActivity(RegisterActivity.class);
+//                break;
+//        }
+//    }
 
     //登录按钮点击了
     @OnClick(R.id.bt_login)
     public void onLoginClick() {
+        Log.d(TAG, "onClick: login");
         startActivity(LoginActivity.class);
+    }
+
+    //注册按钮点击了
+    @OnClick(R.id.bt_register)
+    public void onRegisterClick() {
+        Log.d(TAG, "onClick: register");
+        startActivity(RegisterActivity.class);
     }
 }
