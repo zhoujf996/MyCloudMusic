29ddf9524154f1924f15263efa9f4dd67066d50f
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-07-18 13:20:47 +0800

    3564 Fix toolbar text color bug

--- app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/BaseTitleActivity.java
+++ app/src/main/java/com/ixuea/cources/mycloudmusicproject/activity/BaseTitleActivity.java
@@ -1,8 +1,12 @@
 package com.ixuea.cources.mycloudmusicproject.activity;
 
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 
 import com.ixuea.cources.mycloudmusicproject.R;
+
 import butterknife.BindView;
 
 //通用标题界面
@@ -16,5 +20,34 @@ public class BaseTitleActivity extends BaseCommonActivity {
         super.initViews();
         //初始化Toolbar
         setSupportActionBar(toolbar);
+
+        //是否显示返回按钮
+        if (isShowBackMenu()) {
+            //显示返回按钮
+            showBackMenu();
+        }
+    }
+
+    //显示返回按钮
+    protected void showBackMenu() {
+        //设置为 显示返回按钮
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    //是否显示返回按钮
+    protected boolean isShowBackMenu() {
+        return true;
+    }
+
+    //菜单点击了回调
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            //Toolbar返回按钮点击 固定写法
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
