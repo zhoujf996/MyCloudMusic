7266cfb849376b3923bd34c3fbf9ba8f64e695c9
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-07-19 15:21:40 +0800

    3577 Add stirng util test

---
+++ app/src/main/java/com/ixuea/cources/mycloudmusicproject/util/StringUtil.java
@@ -0,0 +1,11 @@
+package com.ixuea.cources.mycloudmusicproject.util;
+
+//字符串相关工具类
+public class StringUtil {
+
+    //是否是手机号
+    public static boolean isPhone(String value){
+        //实现手机号格式判断
+        return false;
+    }
+}
