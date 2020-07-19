623419ab4fa571ed953487bc2f91941ab0cb72f5
zhoujianyu <zjy_zhoujianyu@yeah.net>
2020-07-19 22:16:01 +0800

    3578 Add check phone format

--- app/src/main/java/com/ixuea/cources/mycloudmusicproject/util/StringUtil.java
+++ app/src/main/java/com/ixuea/cources/mycloudmusicproject/util/StringUtil.java
@@ -1,11 +1,13 @@
 package com.ixuea.cources.mycloudmusicproject.util;
 
+import static com.ixuea.cources.mycloudmusicproject.util.Constant.REGEX_PHONE;
+
 //字符串相关工具类
 public class StringUtil {
 
     //是否是手机号
     public static boolean isPhone(String value){
         //实现手机号格式判断
-        return false;
+        return value.matches(REGEX_PHONE);
     }
 }
